9bdaf5b8765fb3ff62778dc8b1e26cf5bd7dbe1b
tainasilvak <tainacunha42@gmail.com>
2023-08-21 17:02:03 -0300

    adicionei uma função

--- script.js
+++ script.js
@@ -3,18 +3,18 @@ let input = document.querySelector('input')
 let paragraph = document.querySelector('p')
 
 const myContacts = [
-    {name: 'Bill', phoneNumber: '(99) 99999-9999'},
-    {name: 'Job', phoneNumber: '(99) 99999-9999'},
-    {name: 'Naoki', phoneNumber: '(99) 99999-9999'},
-    {name: 'Beatriz', phoneNumber: '(99) 99999-9999'},
-    {name: 'Júlia', phoneNumber: '(99) 99999-9999'},
-    {name: 'Rútila', phoneNumber: '(99) 99999-9999'}
+    {name: 'Bill', phoneNumber: '(19) 99345-9089'},
+    {name: 'Job', phoneNumber: '(21) 99672-0951'},
+    {name: 'Naoki', phoneNumber: '(53) 99233-4321'},
+    {name: 'Beatriz', phoneNumber: '(11) 99422-1271'},
+    {name: 'Júlia', phoneNumber: '(22) 99903-2019'},
+    {name: 'Rútila', phoneNumber: '(25) 99321-9922'}
 
 ]
 
 function search(){
     for(let i = 0; i < myContacts.length; i++){
-        if(input.value === myContacts[i].name){
+        if(input.value.toLowerCase() === myContacts[i].name.toLowerCase()){
             paragraph.innerHTML = `Contato encontrado Nome: ${myContacts[i].name} e o número de telefone é: ${myContacts[i].phoneNumber}`
 
             break
